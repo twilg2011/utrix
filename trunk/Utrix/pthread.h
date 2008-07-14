c20db6a2de5ad532d0a1ae5b09bc65b2ebba352a
m.casenove <m.casenove@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-07-14 09:38:24 +0000

    Aggiunto dichiarazione funzioni

--- trunk/Utrix/pthread.h
+++ trunk/Utrix/pthread.h
@@ -10,4 +10,13 @@
 
 typedef int pthread_t;
 
-
+extern int       pthread_create(pthread_t *, const pthread_attr_t *, void *(*)(void *), void *);
+extern int       pthread_detach(pthread_t);
+extern pthread_t pthread_self(void);
+extern int       pthread_equal(pthread_t, pthread_t);
+extern int       pthread_yield_np(void);
+extern void      pthread_exit(void *);
+extern int       pthread_join(pthread_t, void **);
+extern int       pthread_once(pthread_once_t *, void (*)(void));
+extern int       pthread_sigmask(int, const sigset_t *, sigset_t *);
+extern int       pthread_kill(pthread_t, int);
