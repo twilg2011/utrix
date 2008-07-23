9388735241812ad12014914b6bbbee42094c9882
AlanTuring7 <AlanTuring7@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-07-23 08:03:30 +0000

    aggiunto file per cambio contesto

---
+++ trunk/Utrix/pth_context.h
@@ -0,0 +1,18 @@
+/*
+ *  pth_context.h
+ *  
+ *
+ *  Created by lorenzo galeotti on 23/07/08.
+ *  Copyright 2008 Utrix. All rights reserved.
+ *
+ */
+
+
+
+
+#include "pth_struct.h"
+
+
+context_t* pth_init(void (*f) (void*),void* arg);
+
+int pth_start(context_t* old, context_t* next);
