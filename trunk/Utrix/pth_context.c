e644d8521e800eed0329c31f06ba045bde7b9479
AlanTuring7 <AlanTuring7@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-07-23 08:46:53 +0000

    implementato da finire

--- trunk/Utrix/pth_context.c
+++ trunk/Utrix/pth_context.c
@@ -8,4 +8,43 @@
  */
 
 #include "pth_context.h"
+#include "setjmp.h"
+#include <stdlib.h>
 
+
+void setsp(char* sp);
+void setbp(char* bp);
+
+context_t* pth_init(void (*f) (void*),void* arg)
+{
+  context_t* context=malloc(sizeof(context_t));
+  context->eseguito=0;
+  context->f=(*f);
+  context->arg=arg;
+  return context;
+}
+
+int pth_start(context_t* old, context_t* next)
+{
+  old->ctrlbit=0;
+  setjmp(old->regs);
+  if (old->ctrlbit) return NOERR;
+  if (!(next->eseguito) && !(old->ctrlbit))
+  {
+    char* sp,bp;
+	old->ctrlbit=1;
+    next->eseguito=1;
+	spcalc(&sp,&bp);
+	setsp(sp);
+	setbp(bp);
+	(next->f)(next->arg);
+	return NOERR;
+  }
+  old->ctrlbit=1;
+  longjmp(next->regs);
+}
+
+spclac(char* sp, char* bp)
+{
+
+}
