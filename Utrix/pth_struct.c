7f2281ce79018dddac865f6148f1b17f9a28d31b
c.grioli <c.grioli@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-07-26 13:55:21 +0000

    modifiche a pth_struct

--- Utrix/pth_struct.c
+++ Utrix/pth_struct.c
@@ -11,6 +11,7 @@
 int thread_n;
 int tcb_n = 0; /* Progressive tcb number. 
 				* The tcb_n 0 is assigned to main */
-int thread_exec;
-tcb_t* thread_zombie;
+tcb_t thread_exec=NULL;
+tbl_field_t thread_zombie=NULL;
+list_field_t join_wait=NULL;
 tbl_field_t pth_prior_table[3];
