f216be74b3004ddb28108d97ec2cba6c94359960
c.grioli <c.grioli@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-07-27 10:50:02 +0000

    aggiunta una funzione della libreria

--- trunk/Utrix/pthread_lib.h
+++ trunk/Utrix/pthread_lib.h
@@ -8,4 +8,5 @@ extern int       pthread_yield(void);
 extern void      pthread_exit(void *);
 extern int       pthread_join(pthread_t, void **);
 extern int       pthread_kill(pthread_t, int);
+extern pthread_t pthread_self(void);/*Ritorna il tid del thread che la chiama ho pensato di aggiungerla perchè utile che ne dite?*/
 
