271b89abc8a5f14160b9dcc19cb7f3506ef8a320
m.casenove <m.casenove@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-08-04 09:27:07 +0000

    Elimina l'inizializzazione delle funzioni

--- trunk/Utrix/pthread_lib.h
+++ trunk/Utrix/pthread_lib.h
@@ -1,12 +1,2 @@
-#include "pthread.h"
-/*Qui ci saranno solo le funzioni della libreria*/
 
-extern int       pthread_create(pthread_t *,/* const pthread_attr_t*,*/ void* (*)(void *), void *);
-extern int       pthread_detach(pthread_t);
-extern int       pthread_yield(void);
-
-extern void      pthread_exit(void *);
-extern int       pthread_join(pthread_t, void **);
-extern int       pthread_kill(pthread_t, int);
-extern pthread_t pthread_self(void);/*Ritorna il tid del thread che la chiama ho pensato di aggiungerla perchè utile che ne dite?*/
 
