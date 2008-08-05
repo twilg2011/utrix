c9808e42fb4e0b9fa5e6e8629303e44f23dd0a75
AlanTuring7 <AlanTuring7@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-08-05 10:52:02 +0000

    interfaccia  scheduler

--- trunk/Utrix/pthread_sched.c
+++ trunk/Utrix/pthread_sched.c
@@ -10,15 +10,48 @@
 #include "pthread_sched.h"
 #include <stdlib>
 #include <time.h>
+#include <clock.h>
 
+tbl_field_t  thread_priortail[NUM_PRIOR];
+tbl_field_t  thread_priorhead[NUM_PRIOR];
+tbl_field_t  thread_blocked[NUM_WHY];
 
-tbl_field_t thread_priortail[NUM_PRIOR];
-tbl_field_t  thread_priorhead[NUM_PRIOR];;
 context_t sched;
 
  
 int scheduledthr_n;
 clock_t time;
+
+int serchonlist(int tid, tbl_field_t list,tcb_t* tcb,tcb_t* parent)
+{
+   (*parent)=NULL;
+   (*tcb)=list;
+   while((*tcb))
+   {
+	 if ((*tcb)->tid==tid) return 1;
+	 (*parent)=(*tcb);
+	 (*tcb)=(*tcb)->next;
+   } 
+}
+
+
+int serch(int tid, tcb_t* tcb,tcb_t* parent)
+{ int i=0;
+  while (i<NUM_PRIOR)
+  {
+    if (serchonlist(tid,thread_priortail[PRIOR(i)],tcb,parent)) return 1;
+	i++;
+  }
+  i=0;
+  while (i<NUM_WHY)
+  {
+    if (serchonlist(tid,thread_blocked[i],tcb,parent)) return 1;
+	i++; 
+  }
+  return 0;
+}
+
+
 
 void scheduler(void* arg)
 {
@@ -85,9 +118,32 @@ void recalcprior(tbl_field_t thr)
 }
 
 
-void thrkill(int  tid)
+void schedthrkill(int tid)
 {
- tbl_field_t kill;
- SERCH(tid,thr);
- 
+ tbl_field_t kill,parent;
+ if (!serch(tid,&kill,&parent)) {
+ SETERR(ERRTID);
+ return;
+ }
+ parent->next=kill->next;
+ scheduledthr_n--;
 }
+
+void sleep(int tid,int why){
+}
+
+void unsleep(int tid,int why){
+}
+
+
+tcb_t gettcb(int tid){
+tbl_field_t tcb,parent;
+if (!serch(tid,&tcb,&parent)){
+SETERR(ERRTID);
+return NULL;
+}
+return tbc;
+}
+
+
+
