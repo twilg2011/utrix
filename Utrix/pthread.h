545fdee005a34da6d36d753f0884d9a4eabd329d
c.grioli <c.grioli@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-08-07 14:00:58 +0000

    Modifiche ulteriori

--- Utrix/pthread.h
+++ Utrix/pthread.h
@@ -11,18 +11,31 @@
 #include<stddef.h>
 #include<stdlib.h>
 #include<time.h>
+#include<unistd.h>
+#include<errno.h>
 
-
+struct pthread_mutex_s;
+struct pthread_cond_s;
 typedef unsigned int pthread_t;
+typedef struct pthread_mutex_s pthread_mutex_t;
+typedef struct pthread_cond_s pthread_cond_t ;
+
+
+
+
 
 extern int       pthread_create(pthread_t *,/* const pthread_attr_t*,*/ void* (*)(void *), void *);
 extern int       pthread_detach(pthread_t);
 extern int       pthread_yield(void);
-
 extern void      pthread_exit(void *);
 extern int       pthread_join(pthread_t, void **);
 extern int       pthread_kill(pthread_t, int);
 extern pthread_t pthread_self(void);/*Ritorna il tid del thread che la chiama ho pensato di aggiungerla perchè utile che ne dite?*/
+extern int 	 pthread_mutex_init(pthread_mutex_t* mutex/*, const pthread_mutexattr_t * attr*/);
+extern int 	 pthread_mutex_destroy(pthread_mutex_t* mutex);
+extern int	 pthread_mutex_lock(pthread_mutex_t *mutex);
+extern int 	 pthread_mutex_unlock(pthread_mutex_t *mutex);
+
 
 
 
