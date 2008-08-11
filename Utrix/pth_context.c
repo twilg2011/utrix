edc473af1d4b06eff1ad4842c26b8cf1bf45ed56
AlanTuring7 <AlanTuring7@6950c929-ee51-0410-bf5e-7159ac58be70>
2008-08-11 11:23:43 +0000

    scheduler funzionante da pulire e da rifare la empty

---
+++ Utrix/pth_context.c
@@ -0,0 +1,78 @@
+/*
+ *  pth_context.c
+ *  
+ *
+ *  Created by lorenzo galeotti on 23/07/08.
+ *  Copyright 2008 Utrix. All rights reserved.
+ *
+ */
+#include "pth_context.h"
+#include "pth_errno.h"
+#include "config.h"
+#include <stdlib.h>
+
+partition_t partitionhead;
+partition_t partitiontail;
+int thread_n;
+char* globalsp;
+
+partition_t findfree();
+
+char* bpcalc(context_t ctx)
+{
+   partition_t part=NULL;
+   /*cerco partizioni libere*/
+   if (!findfree()) 
+   {
+     /*se non le trovo ne creo una*/
+     part=malloc(sizeof(partition_s));
+     if (addpar(part)) return NULL;
+   }
+   part->present=1;
+   ctx->part=part;
+   /*ritorno la partizione da usare*/
+   return part->bp;
+}
+
+/*torna 1 se non esistono partizioni*/ 
+int isempty(){
+return !partitionhead;
+}
+
+/*se è possibile aggiunge una partizione valida*/
+int addpar(partition_t new)
+{
+  /*controla la correttezza dei dati*/
+  if (!new || !globalsp) return ERRARG;
+    /*se sono presenti altre partizioni*/
+	if (isempty()){
+	                partitiontail=new;
+	                partitionhead=partitiontail;
+    }else partitionhead->next=new;
+	/*inizializza i dati delle partizioni*/
+	new->next=NULL;
+	new->bp=globalsp-STACKWIDTH;
+	new->present=1;
+	globalsp=new->bp;
+	return NOERR;
+}
+
+/*cerca una partizione libera*/
+partition_t findfree()
+{
+  partition_t res=partitionhead;
+  /*cicla fino a trovare una partizione libera*/
+  while (res)
+  {
+    if(!res->present) return res;
+    res=res->next;
+  }
+  return NULL;
+}
+
+/*libera la partizione passata come argomento*/
+int relasepart(partition_t part)
+{
+  if (part) part->present=0;
+  else ERRARG;
+}
