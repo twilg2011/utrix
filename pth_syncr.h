f361aaf3bec086646de21778bcc9fab529a07fef
m.casenove <m.casenove@6950c929-ee51-0410-bf5e-7159ac58be70>
2009-03-08 19:06:19 +0000

    reintentato

--- pth_syncr.h
+++ pth_syncr.h
@@ -9,27 +9,27 @@
 
 /*Struttura necessaria per capire chi sta in attesa*/
 typedef struct mutexWait{
-pthread_t own;
-struct mutexWait* next;
+	pthread_t own;
+	struct mutexWait* next;
 } mutexWait;
 
 
 /*Struttura del mutex vero e proprio*/
 typedef struct mutex{
-unsigned char state;
-unsigned int val;/*Valore del mutex*/
-pthread_t own;/*Tid di chi possiede il mutex*/
-mutexWait* list_head;
-mutexWait* list_tail;
-struct mutex* next;
-struct mutex* prev;
+	unsigned char state;
+	unsigned int val;/*Valore del mutex*/
+	pthread_t own;/*Tid di chi possiede il mutex*/
+	mutexWait* list_head;
+	mutexWait* list_tail;
+	struct mutex* next;
+	struct mutex* prev;
 } pth_mutex_t;
 
 
 /*Struttura più esterna del mutex, è un semplice contenitore*/
 struct pthread_mutex_s{
-pth_mutex_t* mux;
-int active;
+	pth_mutex_t* mux;
+	int active;
 };
 
 
@@ -37,26 +37,26 @@ typedef struct pthread_mutex_s pthread_mutex_t;
 
 /*Struttura necessaria per memorizzare una lista di condition*/
 typedef struct el_cond{
-pthread_mutex_t* mux;
-pthread_t own;
-struct el_cond* next;
+	pthread_mutex_t* mux;
+	pthread_t own;
+	struct el_cond* next;
 } el_cond_t;
 
 
 /*Struttura effettiva di una condition*/
 typedef struct cond{
-unsigned int state;
-el_cond_t* list_head;
-el_cond_t* list_tail;
-struct cond* next;
-struct cond* prev;
+	unsigned int state;
+	el_cond_t* list_head;
+	el_cond_t* list_tail;
+	struct cond* next;
+	struct cond* prev;
 } pth_cond_t;
 
 
 /*Struttura più esterna della condition, è un semplice contenitore*/
 struct pthread_cond_s{
-pth_cond_t* condition;
-int active;
+	pth_cond_t* condition;
+	int active;
 };
 
 
@@ -70,4 +70,4 @@ extern pth_mutex_t* list_mux;
 extern pth_cond_t* list_cond;
 
 
- 
+
