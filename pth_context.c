aa0bdae2273de3c8b9dc8a16558995e61f07c72d
AlanTuring7 <AlanTuring7@6950c929-ee51-0410-bf5e-7159ac58be70>
2009-03-08 22:14:08 +0000

    modificato

--- pth_context.c
+++ pth_context.c
@@ -15,7 +15,8 @@ partition_t partitionhead;
 partition_t partitiontail;
 int thread_n;
 char* globalsp;
-//int val=0;
+
+
 
 
 partition_t findfree();
@@ -24,53 +25,52 @@ int addpar(partition_t new);
 char* bpcalc(context_t ctx)
 {
     partition_t part=NULL;
-   /*cerco partizioni libere*/
-   if (!(part=findfree())) 
-   {
-     /*se non le trovo ne creo una*/
-     #ifdef DEBUG
-       printf("nuova partizione\n");
-     #endif
-     part=malloc(sizeof(partition_s));
-     if (addpar(part)) return NULL;
-   }
+	/*cerco partizioni libere*/
+	if (!(part=findfree())) 
+	{
+		/*se non le trovo ne creo una*/
 #ifdef DEBUG
-   printf("sp partizione:%p\n",part->bp);
-   
+		printf("nuova partizione\n");
 #endif
-   part->present=1;
-   ctx->part=part;
-   /*ritorno la partizione da usare*/
-   return part->bp;
+		part=malloc(sizeof(partition_s));
+		if (addpar(part)) return NULL;
+	}
+#ifdef DEBUG
+	printf("sp partizione:%p\n",part->bp);
+#endif
+	part->present=TRUE;
+	ctx->part=part;
+	/*ritorno la partizione da usare*/
+	return part->bp;
 }
 
 /*torna 1 se non esistono partizioni*/ 
 int isempty(){
-return !partitionhead;
+	return !partitionhead;
 }
 
-/*se è possibile aggiunge una partizione valida*/
+/*se è possibile aggiunge una partizione valida alla lista*/
 int addpar(partition_t new)
 {
-  /*controla la correttezza dei dati*/
-  if (!new || !globalsp) return EINVAL;
-    /*se sono presenti altre partizioni*/
+	/*controla la correttezza dei dati*/
+	if (!new || !globalsp) return EINVAL;
+    /*se non sono presenti altre partizioni*/
 	if (isempty()){
-	                partitiontail=new;
-                    new->next=NULL;
-	                partitionhead=partitiontail;
+		partitiontail=new;
+		new->next=NULL;
+		partitionhead=partitiontail;
     }else{
         new->next=partitionhead;
         partitionhead=new;
 #ifdef DEBUG
         printf("Num part %d\n",++val);
 #endif
-   
-   
+		
+		
     }
 	/*inizializza i dati delle partizioni*/
 	new->bp=globalsp-STACKWIDTH;
-	new->present=1;
+	new->present=TRUE;
 #ifdef DEBUG
     printf("sp:%p",new->bp);
 #endif
@@ -81,27 +81,26 @@ int addpar(partition_t new)
 /*cerca una partizione libera*/
 partition_t findfree()
 {
-  partition_t res=partitionhead;
-  /*cicla fino a trovare una partizione libera*/
-  while (res)
-  {
-
-    #ifdef DEBUG
-    printf("cerca ancora:%p\n",res->bp);
-    #endif
-    if(!res->present)return res;
-    res=res->next;
-  }
-  return NULL;
+	partition_t res=partitionhead;
+	/*cicla fino a trovare una partizione libera*/
+	while (res)
+	{
+#ifdef DEBUG
+		printf("cerca ancora:%p\n",res->bp);
+#endif
+		if(!res->present)return res;
+		res=res->next;
+	}
+	return NULL;
 }
 
 /*libera la partizione passata come argomento*/
 int releasepart(partition_t part)
 {
-  #ifdef DEBUG
-  printf("relasepart %p\n",part->bp);
-  #endif
-  if (part) part->present=0;
-  else return SETERR(EINVAL);
-  return OK;
+#ifdef DEBUG
+	printf("relasepart %p\n",part->bp);
+#endif
+	if (part) part->present=FALSE;
+	else return SETERR(EINVAL);
+	return OK;
 }
